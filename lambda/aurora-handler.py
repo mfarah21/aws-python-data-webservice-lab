1dfbdcece42b5386c0de97cd418761e33e01aca3
TomCavanaugh <cavanaugh.tom@principal.com>
2021-01-04 19:07:35 +0000

    Cleanup Aurora web service

--- lambda/aurora-handler.py
+++ lambda/aurora-handler.py
@@ -104,7 +104,7 @@ def handler(event, context):
     return {
         'statusCode': 200,
         'headers': {
-            'Content-Type': 'text/plain'
+            'Content-Type': 'application/json'
         },
         'body': responseBody
     }
